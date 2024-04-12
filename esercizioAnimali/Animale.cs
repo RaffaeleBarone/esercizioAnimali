1baadf20971ba6d36bce13b52b2a18c028eafc09
Raffaele <baroneraffaele97@hotmail.com>
2024-04-12 14:49:47 +0200

    aggiunta metodi virtuali commentati come possibile altra implementazione

--- esercizioAnimali/Animale.cs
+++ esercizioAnimali/Animale.cs
@@ -3,3 +3,17 @@
     public abstract void Verso();
     public abstract void Muoversi();
 }
+
+
+//public class Animale
+//{
+//    public virtual void Verso()
+//    {
+//        Console.WriteLine("L'animale fa questo verso:");
+//    }
+
+//    public virtual void Muoversi()
+//    {
+//        Console.WriteLine("L'animale si muove");
+//    }
+//}
